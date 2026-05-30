a01c689d6c16dadcb0190e03dc66ebc28ddd1b2f
TayJyunWey <tayjyunwey@gmail.com>
2026-05-30 15:28:19 +0800

    removed explore tab

--- src/components/app-tabs.tsx
+++ src/components/app-tabs.tsx
@@ -20,13 +20,6 @@ export default function AppTabs() {
           renderingMode="template"
         />
       </NativeTabs.Trigger>
-      <NativeTabs.Trigger name="explore">
-        <NativeTabs.Trigger.Label>Explore</NativeTabs.Trigger.Label>
-        <NativeTabs.Trigger.Icon
-          src={require('@/assets/images/tabIcons/explore.png')}
-          renderingMode="template"
-        />
-      </NativeTabs.Trigger>
       <NativeTabs.Trigger name="profile">
         <NativeTabs.Trigger.Label>Profile</NativeTabs.Trigger.Label>
         <NativeTabs.Trigger.Icon
