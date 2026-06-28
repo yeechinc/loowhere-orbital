005dc98358b618b8bc3da8e5d2c9bbdbe6e4ba93
chen yee chin <marcuschenyc@gmail.com>
2026-06-28 15:38:10 +0800

    login page fixes

--- src/app/login.tsx
+++ src/app/login.tsx
@@ -10,7 +10,7 @@ import {
 } from "react-native";
 import { supabase } from "../../supabaseConfig";
 
-export default function LoginScreen({ onSwitchToRegister, onSuccess }) {
+export default function LoginScreen({ onSwitchToRegister, onSuccess }: { onSwitchToRegister: () => void, onSuccess: () => void }) { 
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [loading, setLoading] = useState(false);
